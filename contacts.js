e7bf72fb4fdb08f164fffcc0865e7b3a24042965
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-03-09 19:09:17 +0200

    майже фініш

---
+++ contacts.js
@@ -0,0 +1,58 @@
+const fs = require("fs").promises;
+
+const path = require("path");
+
+const contactsPath = path.join("./db", "contacts.json");
+// const contactsPath = path.join(__dirname, "./db/contacts.json");
+// console.log(contactsPath)
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+    fs.readFile(contactsPath, (err, data) => {
+        if (err) throw err;
+        console.table(JSON.parse(data));
+    });
+}
+
+function getContactById(contactId) {
+    fs.readFile(contactsPath, (err, data) => {
+        if (err) throw err;
+        const parsedData = JSON.parse(data);
+        const filteredData = parsedData.filter(item => item.id === contactId);
+        console.table(filteredData);
+    });
+}
+
+function removeContact(contactId) {
+    fs.readFile(contactsPath, (err, data) => {
+        if (err) throw err;
+        const allContacts = JSON.parse(data);
+        const filteredContacts = JSON.stringify(allContacts.filter(item => item.id !== contactId));
+        fs.writeFile(contactsPath, filteredContacts, err => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+            console.table(JSON.parse(filteredContacts));
+        });
+    });
+}
+
+function addContact(name, email, phone) {
+    fs.readFile(contactsPath, (err, data) => {
+        if (err) throw err;
+        const oldContactsArr = JSON.parse(data);
+        const newContact = { id: shortid.generate(), name, email, phone };
+        const newContactsArr = JSON.stringify([...oldContactsArr, newContact]);
+        fs.writeFile(contactsPath, newContactsArr, err => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+            console.table(JSON.parse(newContactsArr));
+        });
+    });
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
